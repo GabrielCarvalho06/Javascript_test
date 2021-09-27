74f5d747537cb8020a3b1675052a0b65ccf8fefb
Gabriel Carvalho <gabriel.carvalho@soulasalle.com.br>
2021-09-27 13:24:52 -0300

    Alguns tipos de dados

---
+++ tipos_dados.js
@@ -0,0 +1,9 @@
+var nome = "Gabriel";
+var idade = 26;
+var salvo = true;
+var cidade;
+
+window.alert(typeof nome);    
+window.alert(typeof idade);
+window.alert(typeof salvo);
+window.alert(typeof cidade);
