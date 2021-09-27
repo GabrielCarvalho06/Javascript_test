b6d032eea67bf75fa98a1d109552dc282da8794c
Gabriel Carvalho <gabriel.carvalho@soulasalle.com.br>
2021-09-27 01:08:51 -0300

    Adicionando exercicio de variaveis 1

---
+++ variaveis1.js
@@ -0,0 +1,15 @@
+/*var idade = 26;
+alert(idade);
+
+var nome = "Gabriel Carvalho";
+alert(nome)
+
+var x = 10;
+var y = 15;
+var total = x + y;
+alert(total);
+
+var nome = "Gabriel";
+var sobrenome = "Carvalho";
+var nomecompleto = nome + " " + sobrenome;
+alert(nomecompleto);*/
