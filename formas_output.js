cd8a6b27eb2773139769bfde7e85ba2ab69f0169
Gabriel Carvalho <gabriel.carvalho@soulasalle.com.br>
2021-09-27 00:53:27 -0300

    Formar de output para o usuário

---
+++ formas_output.js
@@ -0,0 +1,15 @@
+//Mensagem para o usuário em forma de document
+
+document.getElementById("titulo").innerHTML = "Tudo bom?";
+document.write("Teste");
+
+//Mensagem para o usuário em forma de window
+
+window.alert("Teste de mensagem com Window");
+
+//Mensagem para o usuário em forma de console
+
+console.log("A mensagem exibida será feita no console");
+
+
+
