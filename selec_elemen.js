c187f9acdcbafc4d051da24a4d3edef50e75b7e0
Gabriel Carvalho <gabriel.carvalho@soulasalle.com.br>
2021-09-27 10:24:33 -0300

    Formas de selecionar elementos com JS

---
+++ selec_elemen.js
@@ -0,0 +1,10 @@
+//Selecionando elementos
+document.getElementById('exemplo').innerHTML = "Trocando texto por id";
+
+document.getElementsByClassName('lista')
+
+document.getElementsByClassName('lista')[0].innerHTML = "Alterando texto pela classe";
+
+document.getElementsByTagName('button')
+
+document.getElementsByName('')
