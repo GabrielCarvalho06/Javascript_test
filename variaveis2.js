634da498b9e87b95aacfdc348f4f30ad61296943
Gabriel Carvalho <gabriel.carvalho@soulasalle.com.br>
2021-09-27 01:45:22 -0300

    Adicionando exercicio 2 de variáveis

---
+++ variaveis2.js
@@ -0,0 +1,12 @@
+//VAR - pode ser usada em forma de output por console ou window
+var nome = "Gabriel";
+
+//LET - Fica disponível somente nesse escopo do código e ela não pode ser redefinida
+if (nome == "Gabriel"){
+    let idade = 26;
+    console.log(idade)
+}
+
+//CONST - É uma variável de valor constante, não pode alterar a estrutura da variável
+const nome2 = "Gabriel";
+console.log(nome2);
