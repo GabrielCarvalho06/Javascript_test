b63ea1971877419383b8c6e509f64ddbda957bde
Gabriel Carvalho <gabriel.carvalho@soulasalle.com.br>
2021-09-27 01:27:16 -0300

    Adicionando condicionais

---
+++ condicionais.js
@@ -0,0 +1,13 @@
+var hora = 18;
+
+if (hora < 12){
+    console.log("Bom dia");
+}
+
+if (hora >= 12 && hora < 18){
+    console.log("Boa tarde");
+}
+
+if (hora >= 18){
+    console.log("Boa noite");
+}
