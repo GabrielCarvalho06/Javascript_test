345bfa27d7e823acf4d4e2f0f750201c6a10eaac
Gabriel Carvalho <gabriel.carvalho@soulasalle.com.br>
2021-10-08 18:25:34 -0300

    Entendendo o switch

--- condicionais.js
+++ condicionais.js
@@ -1,3 +1,4 @@
+//if
 var hora = 18;
 
 if (hora < 12){
@@ -11,3 +12,37 @@ if (hora >= 12 && hora < 18){
 if (hora >= 18){
     console.log("Boa noite");
 }
+
+//Switch
+
+let dia = 3;
+let diaNome = '';
+
+switch(dia) {
+    case 1:
+        diaNome = 'Segunda-Feira';
+        break;
+    case 2:
+        diaNome = 'Terça-Feira';
+        break;
+    case 3:
+        diaNome = 'Quarta-Feira';
+        break;
+    case 4:
+        diaNome = 'Quinta-Feira';
+        break;
+    case 5:
+        diaNome = 'Sexta-Feira';
+        break;
+    case 6:
+        diaNome = 'Sábado';
+        break;
+    case 7:
+        diaNome = 'Domingo';
+        break;
+    default:
+        diaNome = 'Indefinido';
+        break;
+}
+
+document.getElementById("dia").innerHTML = "Hoje é: "+diaNome;
