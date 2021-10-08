a9907ec2dbc58df61db6f24ee6a390157ec1716f
Gabriel Carvalho <gabriel.carvalho@soulasalle.com.br>
2021-10-08 17:07:44 -0300

    objeto1.js

---
+++ objetos1.js
@@ -0,0 +1,15 @@
+//array
+let carros = ["palio", "Uno", "Ferrari", "Corolla"];
+
+
+//Objeto
+let carro = {
+    nome: 'Fiat',
+    modelo: 'Uno',
+    peso: '800kg',
+    ligar: function (){
+        console.log("Carro ligado");
+    }
+};
+
+carro.ligar();
