5ea9b3eb53057173a75b71713dabd3c337369dbd
Gabriel Carvalho <gabriel.carvalho@soulasalle.com.br>
2021-10-08 23:42:55 -0300

    Manipulando números

---
+++ metodo_num.js
@@ -0,0 +1,13 @@
+let n = 10.8364528;
+let num = '25';
+
+let res = n.toString() +5;
+
+res = n.toFixed(3);
+console.log(res);
+
+res = parseInt(num) + 5;
+console.log(res);
+
+res = parseFloat(n) + 5;
+console.log(res);
