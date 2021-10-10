538b64fa7b4d8bac84828f2a6fc19c47ceefb3b0
Gabriel Carvalho <gabriel.carvalho@soulasalle.com.br>
2021-10-09 22:22:54 -0300

    Funções com operações matemáticas

---
+++ mat.js
@@ -0,0 +1,29 @@
+let valor = Math.PI;
+console.log(valor);
+
+valor = Math.round(3.54);
+console.log(valor);
+valor = Math.ceil(3.47);
+console.log(valor);
+
+
+valor = Math.round(3.47);
+console.log(valor);
+valor = Math.floor(3.54)
+console.log(valor);
+
+valor = Math.abs(-9.65);
+console.log(valor);
+
+valor = Math.min(7, 100, 1);
+console.log(valor);
+
+valor = Math.max(7, 100, 1);
+console.log(valor);
+
+valor = Math.random();
+console.log(valor);
+
+valor = Math.floor(Math.random() * 100);
+console.log(valor);
+
