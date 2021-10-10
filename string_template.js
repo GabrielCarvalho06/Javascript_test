54507709346559ebef804e980dccb3cfaf2d0dc0
Gabriel Carvalho <gabriel.carvalho@soulasalle.com.br>
2021-10-09 23:04:50 -0300

    Template string

---
+++ string_template.js
@@ -0,0 +1,9 @@
+let nome = 'Gabriel';
+let idade = 26;
+
+let frase = 'Meu nome é '+nome+' e eu tenho '+idade+' anos.'
+console.log(frase);
+
+frase = `Meu nome é ${nome} e eu tenho ${idade} anos.`
+console.log(frase);
+
