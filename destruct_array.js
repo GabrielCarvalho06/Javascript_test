6f773ef724c59dc5836ff2d55baee20f0c28965d
Gabriel Carvalho <gabriel.carvalho@soulasalle.com.br>
2021-10-18 10:44:27 -0300

    Desconstruindo array

---
+++ destruct_array.js
@@ -0,0 +1,6 @@
+let array = ['Gabriel Carvalho', 'Gabriel', 'Carvalho', '@gabrielscarvalho'];
+
+//let [ nomeCompleto, nome, sobrenome, instagram ] = array;
+let [ nomeCompleto, ,, instagram] = array;
+console.log(nomeCompleto, instagram);
+
