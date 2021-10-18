e7ce4908e83f2d4c100a866a15e16975c8519a11
Gabriel Carvalho <gabriel.carvalho@soulasalle.com.br>
2021-10-18 17:14:08 -0300

    Keys, Values e Entry

---
+++ keys_values_entry.js
@@ -0,0 +1,25 @@
+let lista = [1, 2, 3, 4];
+console.log(typeof lista);
+
+let lista2 = {
+    nome:'Gabriel',
+    sobrenome:'Carvalho'
+};
+
+console.log(typeof lista2);
+
+let lista3 = ['arroz', 'ovo', 'macarrão'];
+
+console.log(Object.keys(lista3));
+console.log(Object.values(lista3));
+console.log(Object.entries(lista3));
+
+let pessoa = {
+    nome:'Gabriel',
+    sobrenome:'Carvalho',
+    idade: 26
+};
+
+console.log(Object.keys(pessoa));
+console.log(Object.values(pessoa));
+console.log(Object.entries(pessoa));
