dc22e00ddb4d801612a52c4aaef6894d4f2bd5d2
Gabriel Carvalho <gabriel.carvalho@soulasalle.com.br>
2021-10-18 17:03:46 -0300

    Includes e Repeat

---
+++ includes_repeat.js
@@ -0,0 +1,9 @@
+let lista = ['carne', 'ovo', 'arroz', 'feijão', 'macarrão'];
+
+if (lista.includes('Carne') == true) {
+    console.log('Tem carne na lista');
+} else {
+    console.log('Não tem carne na lista');
+}
+
+console.log('x'.repeat(20));
